dd268d0cf74650b66dd9f31cda71ac37cc01415b
ishwari2121 <ishwarigondkar9161@gmail.com>
2025-02-19 15:43:07 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-yellow-500'>
+        <h1>hello</h1>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
